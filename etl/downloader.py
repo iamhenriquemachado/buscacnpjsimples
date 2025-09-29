bc1fa20cf6f53ecca07393c81bf3874526afffc8
henrique.castro <heyhenriquecastro@gmail.com>
2025-09-29 06:47:19 -0300

    feat: setup postgres databse structure and create docker image/containter

--- etl/downloader.py
+++ etl/downloader.py
@@ -1,21 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime 
 
-from utils.utils import createCnpjDataDirectory, getCurrentMonthAndYear
+url = f'https://arquivos.receitafederal.gov.br/dados/cnpj/dados_abertos_cnpj/'
 
-get_current_date = getCurrentMonthAndYear()
-
-url = f'https://arquivos.receitafederal.gov.br/dados/cnpj/dados_abertos_cnpj/{get_current_date}/Cnaes.zip'
-response = requests.get(url)
-
-soup = BeautifulSoup(response.text, "html.parser")
-
-links = soup.find_all("a")
-
-# for link in links:
-#     print(link.get("href"))
-
-with open("Cnaes.zip", "w", encoding="utf-8") as file:
-        file.write(response.text)
-
-print("Arquivo Cnaes.zip salvo!")
+current_date = datetime.now().strftime('%d-%m-%Y')
+print(current_date)
